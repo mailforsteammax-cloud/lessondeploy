d98377bbcde316a8b9ee51ce9a28caa07b20d90b
mailforsteammax-cloud <mailforsteammax@gmail.com>
2026-04-27 16:25:15 +0400

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+import asyncio
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import Command
+from dotenv import load_dotenv
+
+load_dotenv()
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("Я жив 🚀")
+
+@dp.message(Command("ping"))
+async def ping(message: types.Message):
+    await message.answer("pong")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
